6cfd834292635fb6d146088865f70500f188c1be
bachanp <Pierre-Olivier.Bachant-Winner@ircm.qc.ca>
2013-05-31 11:18:07 -0400

    enhance Pipeline

--- meta_pipeline.R
+++ meta_pipeline.R
@@ -1,12 +1,15 @@
 rm(list=ls())
 ptm <- proc.time()
 source(file.path("/stockage/homes/bachanp/Function/OpenDataset2.R"))
-gselist <- OpenDataset2(config.fil="GSE_Curation.csv")
+gselist <- OpenDataset2(config.fil="GSE_Curation.csv",rescale.checker=TRUE)
 source(file.path("/stockage/homes/bachanp/Function/SubtypeClassification.R"))
 STL <- SubtypeClassification(gselist=gselist, config.file="GSE_Curation.csv")
 source(file.path("/stockage/homes/bachanp/Function/Merging.R"))
 master.eset <- Merging(gselist=gselist, STL=STL, duplication.checker=TRUE)
+proc.time() - ptm
 
+
+##########################################################################
 #This shall be replace when the survivalscore function will be operational
 ##########################################################################
 survival.score.list <- list()
@@ -19,6 +22,7 @@ for (i in 1:4){
 }
 ##########################################################################
 source(file.path("/stockage/homes/bachanp/Function/GseaAnalysis.R"))
+matrix.subtype <- as.matrix(pData(master.eset))[,20:length(pData(master.eset))]
 gsea.output <- GseaAnalysis(survival.score.list=survival.score.list,gmt.file="c2.all.v3.1.entrez.gmt",matrix.subtype=matrix.subtype)
 
 proc.time() - ptm
