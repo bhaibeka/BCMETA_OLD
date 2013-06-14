6cde94e084ee418d764b509ae9ea7fb5a36cea9e
bachanp <Pierre-Olivier.Bachant-Winner@ircm.qc.ca>
2013-06-14 08:44:59 -0400

    Merging function for different platform

--- OpenDataset2.R
+++ OpenDataset2.R
@@ -1,4 +1,4 @@
-OpenDataset2 <- function(config.file, rescale=FALSE){
+OpenDataset2 <- function(config.file){
   #Open every GSE, specify in the configuration CSV, from InsilicoDB and create a list of Eset
   #structure call gselist
   #
@@ -13,41 +13,47 @@ OpenDataset2 <- function(config.file, rescale=FALSE){
   
   require(inSilicoDb2)
   require(genefu)
+  require(jetset)  
+  source(file.path("/stockage/homes/bachanp/Function/ProbeGeneMapping.R"))
+  source(file.path("/stockage/homes/bachanp/Function/PlatformMerging.R"))
+  
   #login
   InSilicoLogin(login="bhaibeka@gmail.com", password="747779bec8a754b91076d6cc1f700831")
   #File.To.Open (FTO)
   FTO <- read.csv(config.file)   
+  #FTO <- read.csv("GSE_Curation.csv")
   gselist <- list() 
-  load("~/NKIORIGINALPROBE.RData")
-  load("~/METABRICORIGINALPROBE.RData")
-  gselist[[1]] <- METABRICORIGINALPROBE
-  phenoData(gselist[[1]])@data <- cbind(phenoData(gselist[[1]])@data, rep("GPL6947",nrow(phenoData(gselist[[1]])@data)))
-  colnames(phenoData(gselist[[1]])@data)[length(colnames(phenoData(gselist[[1]])@data))] <- "platform"
-  gselist[[2]]<- NKIORIGINALPROBE
-  colnames(phenoData(gselist[[2]])@data)[length(colnames(phenoData(gselist[[2]])@data))] <- "platform"
-  phenoData(gselist[[2]])@data[length(phenoData(gselist[[2]])@data)] <- "AGIR"
-  counter <- 3
+  counter <- 1
   
   #Opening all dataset from InsilicoDB
   for (i in 16:nrow(FTO)){
     if (as.character(FTO[i,4])==TRUE){    
-      GPL <- getPlatforms(dataset=as.character(FTO[i,2]))[1]
-      gselist[[counter]] <- getDataset(dataset=as.character(FTO[i,2]), curation=FTO[i,3], platform=GPL)        
-      counter <- counter+1
+      GPL <- getPlatforms(dataset=as.character(FTO[i,2]))
+      for (j in 1:length(GPL)){
+        gselist[[counter]] <- getDatasets(dataset=as.character(FTO[i,2]), norm=as.character(FTO[i,6]), curation=FTO[i,3], features="PROBE")[[j]]       
+        #making probe gene mapping for Affy structure
+        if (as.character(FTO[i,5])=="Affy"){            
+          gselist[[counter]] <- ProbeGeneMapping(gselist[[counter]])
+        } else{        
+          rownames(exprs(gselist[[counter]])) <- paste("geneid.",Biobase::featureData(gselist[[counter]])$ENTREZID, sep="")
+          }
+        if (length(GPL) > 1){
+          index <- length(gselist)-GPL.length+1
+          gselist[[index]] <- PlatformMerging(gselist=gselist, GPL.length=length(GPL))
+          for (k in 1:length(gselist)){
+            gselist[[index+k]] <- NULL
+          }          
+        }
+        counter <- counter +1
+      } 
     }
-  }
-  #Making sure that every rownames for matrix expression is well geneid.#####
-  #and that every column of the clinical data matrix is well aligned for every GSE dataset
-  for (i in 1:length(gselist)){
-    rownames(exprs(gselist[[i]])) <- paste("geneid.",as.vector(Biobase::featureData(gselist[[i]])$ENTREZID),sep="")
+  }    
+  
+  #Making sure that every column of the clinical data matrix is well aligned for every GSE dataset
+  for (i in 1:length(gselist)){    
     phenoData(gselist[[i]])@data<- phenoData(gselist[[i]])@data[,match(colnames(phenoData(gselist[[1]])@data),colnames(phenoData(gselist[[i]])@data))]    
   }
-  #rescale each dataset individually     
-  if (rescale) {
-    for (i in 1:length(gselist)) {
-      exprs(gselist[[i]]) <- t(((apply(exprs(gselist[[i]]), 1, function(x) { return(genefu::rescale(x, q=0.05, na.rm=TRUE)) })) - 0.5) * 2)
-    }
-  }
+
   #logout
   InSilicoLogout() 
   return(gselist)
---
+++ PlatformMerging.R
@@ -0,0 +1,25 @@
+PlatformMerging <- function(gselist, GPL.length){
+  #Open every GSE, specify in the configuration CSV, from InsilicoDB and create a list of Eset
+  #structure call gselist
+  #
+  # Args:
+  #     gselist: Containt the gselist with every GSE. 
+  #     GPL.length: Is the length of the GPL vector, it specify how many platform that need to be merge
+  #   
+  # Returns:     
+  #     The GSE which each platform has been merge
+  
+  GPL.length=2
+  index <- length(gselist)-GPL.length+1
+  for (i in 1:GPL.length-1){
+    missing.genes <- setdiff(rownames(exprs(gselist[[index+i]])),rownames(exprs(gselist[[index]])))
+    exprs(gselist[[index]]) <- rbind(exprs(gselist[[index]]), exprs(gselist[[index+i]])[match(missing.genes, rownames(exprs(gselist[[index+i]]))), ,drop=FALSE])
+    gene.id <- sapply(missing.genes, function(x){strsplit(x, "\\.")[[1]][2]})
+    index2 <- match(gene.id, Biobase::featureData(gselist[[index+i]])@data[ ,1])
+    gene.annotation.matrix <- Biobase::featureData(gselist[[index+i]])@data[index2, ]
+    colnames(gene.annotation.matrix) <- colnames(Biobase::featureData(gselist[[index]])@data)
+    Biobase::featureData(gselist[[index]])@data <- rbind(Biobase::featureData(gselist[[index]])@data, gene.annotation.matrix)    
+  }
+  return(gselist[[index]])
+}
+  
