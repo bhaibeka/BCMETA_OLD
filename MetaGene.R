2e5e03f90e53d44d3d6e6f05ffe1088877b67dd9
Benjamin Haibe-Kains <bhaibeka@ircm.qc.ca>
2013-06-17 11:59:08 -0400

    Update MetaGene.R

--- MetaGene.R
+++ MetaGene.R
@@ -1,6 +1,6 @@
-
 ## function computing gene-drug associations
-geneset.score <- function(genes, data, model=FALSE, na.rm=FALSE) {
+geneset.score <- function(genes, data, coeffcients, model=FALSE, method=c("principal.component", "weighted.average"), na.rm=FALSE) {
+  method <- match.arg(method)
   n0 <- length(genes)
   genes <- intersect(genes, colnames(data))
   if(length(genes) < n0) { warning(sprintf("%i genes from geneset are not present in the gene expression dataset", n0 - length(genes))) }
@@ -9,13 +9,19 @@ geneset.score <- function(genes, data, model=FALSE, na.rm=FALSE) {
   if(nn > 0 && !na.rm) { stop("missing values are present") }
   cl <- rep(NA, nrow(data))
   names(cl) <- rownames(data)
-  if(n0 > 0 && nn >= 3) {
-    ## compute the first principal component
-    prm <- prcomp(x=data[ccix, genes, drop = FALSE])
-    tt <- prm$x[ , 1]
-    cl[names(tt)] <- tt
-    if(model) { cl <- list("pc1"=cl, "model"=prm) }
-  }
+  switch(method,
+    "principal.component"= {
+      if(n0 > 0 && nn >= 3) {
+        ## compute the first principal component
+        prm <- prcomp(x=data[ccix, genes, drop = FALSE])
+        tt <- prm$x[ , 1]
+        cl[names(tt)] <- tt
+        if(model) { cl <- list("pc1"=cl, "model"=prm) }
+      }
+    },
+    "weighted.average"={
+      ## signature.score
+  })
   return(cl)
 }
 
